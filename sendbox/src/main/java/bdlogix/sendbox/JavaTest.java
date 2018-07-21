add5669cb3454d00f1f1af3078a97626722e80a2
regtstr <41239534+regtstr@users.noreply.github.com>
2018-07-21 18:16:44 +0300

    add new functions

--- sendbox/src/main/java/bdlogix/sendbox/JavaTest.java
+++ sendbox/src/main/java/bdlogix/sendbox/JavaTest.java
@@ -3,7 +3,23 @@ package bdlogix.sendbox;
 public class JavaTest {
 	
 	public static void main(String[] args) {
-		System.out.println("Hellow, world!");
-	}
-		
+		hello("world!");
+
+		double l = 8;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a =6;
+		double b = 8;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+;		}
+
+		public static void hello(String w) {
+					System.out.println("Hellow, " + w);
+		}
+		public static double area (double l) {
+		return l * l;
+		}
+		public static  double area(double a, double b) {
+		return a * b;
+		}
 }
